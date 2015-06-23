cdc04abbb72d9f186733b7a718ca4b5c4415b06a
sgnatonski <slawek.gnatonski@gmail.com>
2015-06-23 17:00:01 +0200

    WinProcessWatcher initial commit

---
+++ WinProcessWatcher/ProcessWatcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlavApp.WinProcessWatcher
+{
+    public class ProcessWatcher
+    {
+        public IWMIWatcher WatchStart(string processName, Action<DateTime> notifyAction)
+        {
+            return new WMIWatcher("Win32_ProcessStartTrace", processName, notifyAction);
+        }
+        public IWMIWatcher WatchStop(string processName, Action<DateTime> notifyAction)
+        {
+            return new WMIWatcher("Win32_ProcessStopTrace", processName, notifyAction);
+        }
+    }
+}
---
+++ WinProcessWatcher/WMIWatcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlavApp.WinProcessWatcher
+{
+    internal class WMIWatcher : IWMIWatcher
+    {
+        private ManagementEventWatcher w = null;
+        private Action<DateTime> notifyAction = t => { };
+        private string processName;
+
+        public WMIWatcher(string eventClassName, string processName, Action<DateTime> notifyAction)
+        {
+            this.notifyAction = notifyAction;
+            this.processName = processName;
+            var q = new WqlEventQuery();
+            q.EventClassName = eventClassName;
+            this.w = new ManagementEventWatcher(q);
+            this.w.EventArrived += new EventArrivedEventHandler(ProcessStartEventArrived);
+            this.w.Start();
+        }
+
+        private void ProcessStartEventArrived(object sender, EventArrivedEventArgs e)
+        {
+            var processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            if (processName.ToLowerInvariant() == this.processName.ToLowerInvariant())
+            {
+                //var timeCreated = (ulong)e.NewEvent.Properties["TIME_CREATED"].Value;
+                this.notifyAction(DateTime.Now);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (this.w != null)
+            {
+                this.w.Stop();
+            }
+        }
+    }
+}
