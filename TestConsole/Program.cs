c21e755702bd09ab0e4581dae97ad900b63ac5e8
sgnatonski <slawek.gnatonski@gmail.com>
2016-07-20 20:29:28 +0200

    TestConsole update

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,10 +1,14 @@
-﻿using SlavApp.ImageFinder;
+﻿using SlavApp.Minion.Resembler.Context.Handlers.Shell;
+using SlavApp.Resembler;
+using SlavApp.Windows;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestConsole
@@ -13,7 +17,23 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            var current = 0.0;
+            if (!ApplicationHelper.CheckApplicationAlreadyRunning("SlavApp.Minion", true) && !ApplicationHelper.CheckApplicationAlreadyRunning("SlavApp.Minion.vshost", true))
+            {
+                var path = Path.GetDirectoryName(Assembly.GetAssembly(typeof(CompareImagesContextMenuHandler)).Location);
+                var psi = new ProcessStartInfo();
+                psi.FileName = Path.Combine(path, "..\\..\\..\\SlavApp.Minion\\bin\\Debug\\SlavApp.Minion.exe");
+                psi.WorkingDirectory = Path.GetDirectoryName(psi.FileName);
+                var process = new Process();
+                process.StartInfo = psi;
+                process.Start();
+                process.WaitForInputIdle();
+            }
+            NamedPipeListener<string>.SendMessage("SlavApp.Minion.Resembler", "boo");
+
+            Console.ReadLine();
+            return;
+
+            /*var current = 0.0;
             var sf = new SimilarHistogramFinder();
             sf.OnCompareProgress += (total, file1, file2, v) =>
             {
@@ -26,7 +46,7 @@ namespace TestConsole
 
             sf.Run(@"R:\APART_ALL\ZDJĘCIA EXPO", "*.jpg", 0.6);
 
-            Console.ReadLine();
+            Console.ReadLine();*/
 
             //Debug.WriteLine(results.Count());
             //results.OrderByDescending(x => x.Value).ToList().ForEach(x => Debug.WriteLine("{0}\t\t{1}\t\t{2} %", Path.GetFileName(x.First), Path.GetFileName(x.Second), x.Value * 100));
