de6a6ae0d161e19e094e4a4204a4e48629a70e7e
sgnatonski <slawek.gnatonski@gmail.com>
2016-07-20 20:28:53 +0200

    ElevatedRunner initial setup

---
+++ SlavApp.Minion.ElevatedRunner/Program.cs
@@ -0,0 +1,36 @@
+﻿using SlavApp.Minion.Resembler.Context.Handlers.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlavApp.Minion.ElevatedRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            if (args[0] == "context")
+            {
+                Console.Write("Context ");
+                if (args[1] == "install")
+                {
+                    Console.WriteLine("installing...");
+                    new CompareImagesContextMenuHandler().Register();
+                }
+                if (args[1] == "uninstall")
+                {
+                    Console.WriteLine("uninstalling...");
+                    new CompareImagesContextMenuHandler().Unregister();
+                }
+            }
+
+            Console.WriteLine("Done. Press any key to close.");
+            Console.ReadLine();
+        }
+    }
+}
