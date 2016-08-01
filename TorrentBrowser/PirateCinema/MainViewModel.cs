677488f3707c90da2e2de390b16f02e253feeeec
Slawomir Gnatonski <slawomir.gnatonski@ciber.com>
2016-08-01 15:17:05 +0200

    improved cancellationToken handling

--- TorrentBrowser/PirateCinema/MainViewModel.cs
+++ TorrentBrowser/PirateCinema/MainViewModel.cs
@@ -13,11 +13,18 @@ namespace PirateCinema
     {
         public MainViewModel()
         {
+            Application.Current.Exit += Current_Exit;
+
             FetchCommand = ReactiveCommand.CreateAsyncObservable(_ => FetchMovies());
 
             var movies = MovieListFactory.Build(new ReactiveList<TorrentMovie>(new TorrentBrowserWorker().GetCache()));
 
             MoviesList = movies.ByRatingWithSubtitle;
+        }
+
+        private void Current_Exit(object sender, ExitEventArgs e)
+        {
+            _cancelToken.Cancel();
         }
 
         public ICommand FetchCommand { get; set; }
@@ -29,20 +36,22 @@ namespace PirateCinema
             set { this.RaiseAndSetIfChanged(ref _moviesList, value); }
         }
 
+        private CancellationTokenSource _cancelToken = new CancellationTokenSource();
+
         public IObservable<Unit> FetchMovies()
         {
+            _cancelToken = new CancellationTokenSource();
+
             var movieList = MovieListFactory.Build();
 
             MoviesList = movieList.ByRatingWithSubtitle;
 
             return Observable.Start(() =>
             {
-                var cancelToken = new CancellationTokenSource();
-
                 var worker = new TorrentBrowserWorker();
 
-                var moviesSource1 = worker.Work(TorrentSiteProvider.PirateBay, SubtitleLanguage.Polish, cancelToken.Token);
-                var moviesSource2 = worker.Work(TorrentSiteProvider.ExtraTorrent, SubtitleLanguage.Polish, cancelToken.Token);
+                var moviesSource1 = worker.Work(TorrentSiteProvider.PirateBay, SubtitleLanguage.Polish, _cancelToken.Token);
+                var moviesSource2 = worker.Work(TorrentSiteProvider.ExtraTorrent, SubtitleLanguage.Polish, _cancelToken.Token);
                 //var moviesSource3 = worker.Work(TorrentSiteProvider.Kat, SubtitleLanguage.Polish, cancelToken.Token);
 
                 var moviesMerged = moviesSource1.Merge(moviesSource2);
